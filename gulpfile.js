af90c2ab571c52c7febd07250916137f9414a1c6
Andre Uschmann <mail@andre-uschmann.de>
2015-12-02 22:49:05 +0100

    added make gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,35 @@ var shell = require('gulp-shell');
 var uglify = require('gulp-uglify');
 var ngTemplates = require('gulp-ng-templates');
 var minifyHtml = require('gulp-minify-html');
+var runSequence = require('run-sequence');
 
 gulp.task('default', function() {
 
 });
 
+gulp.task('make', function() {
+    runSequence('browserify', 'sass', 'view', 'compress', function() {
+        console.log('Run something else');
+    });
+});
+
+/**
+ * Package javascript sources
+ */
 gulp.task('browserify', shell.task([
     'browserify src/js/main.js -o public/js/bundle.js'
 ]));
 
+/**
+ * Compile SCSS
+ */
 gulp.task('sass', shell.task([
     'sass src/scss/style.scss public/css/style.css'
 ]));
 
+/**
+ * Package and copy views
+ */
 gulp.task('view', function() {
     gulp.src('./src/views/index.html')
         .pipe(gulp.dest('./public'));
@@ -33,12 +49,18 @@ gulp.task('view', function() {
         .pipe(gulp.dest('public/js'));
 });
 
+/**
+ * Compresses the js bundle
+ */
 gulp.task('compress', function() {
-    return gulp.src('./src/js/bundle.js')
+    return gulp.src('./public/js/bundle.js')
         .pipe(uglify({mangle: false}))
-        .pipe(gulp.dest('public/js/'));
+        .pipe(gulp.dest('public/js'));
 });
 
+/**
+ * Watch sources
+ */
 gulp.task('watch', function () {
     gulp.watch(['./src/js/**/*.js'], ['browserify']);
     gulp.watch(['./src/scss/**/*.scss'], ['sass']);
--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1,4 +1,4 @@
 'use strict';
 var app = require('angular').module('app');
 
-app.controller('DemoController', require('./TestController'));
+app.controller('DemoController', require('./DemoController'));
